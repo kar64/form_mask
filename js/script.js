00bb338443b7865a74601cc074c30b64577fb8c3
kar64 <140837613+kar64@users.noreply.github.com>
2023-11-09 07:37:49 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,22 +8,27 @@ IMask(document.querySelector("#age"), {
 });
 IMask(document.querySelector("#date"), {
   mask: Date,
-
+  lazy:false,
+  overwrite:true,
+  autofix:true,
   blocks: {
     d: {
       mask: IMask.MaskedRange,
+      placeholderChar:'d',
       from: 1,
       to: 31,
       maxLength: 2,
     },
     m: {
       mask: IMask.MaskedRange,
+      placeholderChar:'m',
       from: 1,
       to: 12,
       maxLength: 2,
     },
     Y: {
       mask: IMask.MaskedRange,
+      placeholderChar:'d',
       from: 1900,
       to: 9999,
     },
